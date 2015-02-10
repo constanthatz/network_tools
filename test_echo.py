f9037fc7325c8b73c9c2819d4cfe10b168da9100
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-10 11:38:43 -0800

    add test fixture to simplify tests.

--- test_echo.py
+++ test_echo.py
@@ -1,34 +1,35 @@
 from echo_client import client_socket_function
 import subprocess
-import os
-import signal
+import pytest
+
+
+@pytest.fixture(scope='module')
+def server(request):
+    """set up and tear down a server"""
+
+    process = subprocess.Popen('./echo_server.py', shell=True)
+
+    def cleanup():
+        process.kill()
+
+    request.addfinalizer(cleanup)
+
+    return process
 
 
 def test_client_socket_function_short():
-    ''' Test client/server interaction. '''
-
-    process = subprocess.Popen('./echo_server.py', shell=True)
-    ''' 16 byte message. '''
+    ''' 16 byte message. Short response'''
     recieve = client_socket_function("Can you hear me?")
     assert recieve == "I recieved your message. Stop talking to me. You are annoying."
-    process.kill()
 
 
 def test_client_socket_function_long():
-    ''' Test client/server interaction. '''
-
-    process = subprocess.Popen('./echo_server.py', shell=True)
     ''' 74 byte message. '''
     recieve = client_socket_function("Can you hear me? I am waiting for you to respond. Come on, where are you?!")
     assert recieve == "I recieved your message. Stop talking to me. You are annoying."
-    process.kill()
 
 
 def test_client_socket_function_unicode():
-    ''' Test client/server interaction. '''
-
-    process = subprocess.Popen('./echo_server.py', shell=True)
     ''' Unicode. '''
     recieve = client_socket_function(u"Can you hear me? I am waiting for you to respond. Come on, where are you?!")
     assert recieve == "I recieved your message. Stop talking to me. You are annoying."
-    process.kill()
