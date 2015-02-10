87232a0bc0f29582557630cbbfce9dd67dea9ede
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-09 23:38:12 -0800

    initialize test file and fix line spacing in client.

--- echo_client.py
+++ echo_client.py
@@ -2,13 +2,12 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 import socket
+import sys
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
-
 client_socket.connect(('127.0.0.1', 50000))
-
-client_socket.sendall("Hey, can you hear me?")
+client_socket.sendall(sys.argv[1])
 client_socket.shutdown(socket.SHUT_WR)
-
-client_socket.recv(32)
+print(client_socket.recv(32))
 client_socket.close()
+
