a9e3176f734485ca97a48b147e2b688c43dec751
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-10 09:16:09 -0800

    change client server into a funciton.

--- echo_client.py
+++ echo_client.py
@@ -4,10 +4,16 @@ from __future__ import unicode_literals
 import socket
 import sys
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
-client_socket.connect(('127.0.0.1', 50000))
-client_socket.sendall(sys.argv[1])
-client_socket.shutdown(socket.SHUT_WR)
-print(client_socket.recv(32))
-client_socket.close()
 
+def client_socket_funciton():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+    client_socket.connect(('127.0.0.1', 50000))
+    client_socket.sendall(sys.argv[1])
+    client_socket.shutdown(socket.SHUT_WR)
+    recieve = client_socket.recv(32)
+    client_socket.close()
+    return recieve
+
+if __name__ == '__main__':
+    recieve = client_socket_funciton()
+    print(recieve)
