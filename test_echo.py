93cc6564d856f59336bf446b3bbb5325f1ed3d08
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-10 09:19:48 -0800

    add first assert to test.

--- test_echo.py
+++ test_echo.py
@@ -1,5 +1,6 @@
-import echo_client
+from echo_client import client_socket_funciton
 
 
-def test_echo():
-    echo_client "Can you hear me?"
+def test_client_socket_function():
+    recieve = echo_client("Can you hear me?")
+    assert recieve == "I received your message."
