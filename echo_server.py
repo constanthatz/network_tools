e58c573164f21256794c02a99df7f5e4cd07d0f5
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-10 09:20:56 -0800

    change server into a funciton.

--- echo_server.py
+++ echo_server.py
@@ -3,12 +3,18 @@ from __future__ import print_function
 from __future__ import unicode_literals
 import socket
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
-server_socket.bind(('127.0.0.1', 50000))
-server_socket.listen(1)
-while True:
-    conn, addr = server_socket.accept()
-    message = conn.recv(32)
-    if message:
-        conn.sendall("I recieved your message.")
-    conn.close()
+
+def server_socker_function():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+    server_socket.bind(('127.0.0.1', 50000))
+    server_socket.listen(1)
+    while True:
+        conn, addr = server_socket.accept()
+        message = conn.recv(32)
+        if message:
+            conn.sendall("I recieved your message.")
+        conn.close()
+
+
+if __name__ == '__main__':
+    server_socker_function()
