f8038804d77682900bdd1ba740131ddb3717b11a
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-09 22:32:35 -0800

    add basic code for server and client.

--- echo_server.py
+++ echo_server.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 from __future__ import print_function
 from __future__ import unicode_literals
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+server_socket.bind(('127.0.0.1', 50000))
+server_socket.listen(1)
+conn, addr = server_socket.accept()
+
+conn.recv(32)
+
+conn.sendall("Yes, I hear you.")
+conn.close()
+
+server_socket.close()
