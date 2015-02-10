0a02f30785f0211a1e3b235f11e45cfd2d23a2db
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-10 10:02:31 -0800

    fix comment.

--- test_echo.py
+++ test_echo.py
@@ -1,6 +1,16 @@
 from echo_client import client_socket_funciton
 
 
-def test_client_socket_function():
-    recieve = echo_client("Can you hear me?")
-    assert recieve == "I received your message."
+def test_client_socket_function_short():
+    ''' Test client/server interaction. '''
+
+    ''' 16 byte message. '''
+    recieve = client_socket_funciton("Can you hear me?")
+    assert recieve == "I recieved your message."
+
+
+def test_client_socket_function_long():
+    ''' Test client/server interaction. '''
+    ''' 74 byte message. '''
+    recieve = client_socket_funciton("Can you hear me? I am waiting for you to respond. Come on, where are you?!")
+    assert recieve == "I recieved your message."
