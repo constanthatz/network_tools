d3f3741d8ca4587d8bce68f625dceca3f2909465
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-10 09:23:55 -0800

    fix function input.

--- echo_client.py
+++ echo_client.py
@@ -5,15 +5,15 @@ import socket
 import sys
 
 
-def client_socket_funciton():
+def client_socket_funciton(message):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
     client_socket.connect(('127.0.0.1', 50000))
-    client_socket.sendall(sys.argv[1])
+    client_socket.sendall(message)
     client_socket.shutdown(socket.SHUT_WR)
     recieve = client_socket.recv(32)
     client_socket.close()
     return recieve
 
 if __name__ == '__main__':
-    recieve = client_socket_funciton()
+    recieve = client_socket_funciton(sys.argv[1])
     print(recieve)
