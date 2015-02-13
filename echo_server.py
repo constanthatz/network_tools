a98451dafe2ee4de6743a6d462949f0af92bb0ae
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-13 01:26:41 -0800

    add loop to continuously listen for a message. add proper echo response.

--- echo_server.py
+++ echo_server.py
@@ -4,16 +4,30 @@ import socket
 
 
 def server_socket_function():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_IP)
+    server_socket = socket.socket(socket.AF_INET,
+                                  socket.SOCK_STREAM,
+                                  socket.IPPROTO_IP)
     server_socket.bind(('127.0.0.1', 50000))
     server_socket.listen(1)
 
     try:
         while True:
             conn, addr = server_socket.accept()
-            message = conn.recv(32)
-            if message:
-                conn.sendall("I recieved your message. Stop talking to me. You are annoying.")
+
+            recieve_total = ""
+            buffersize = 32
+            finished = 0
+            while not finished:
+                recieve = conn.recv(buffersize)
+                if len(recieve) < buffersize:
+                    recieve_total += recieve
+                    finished = 1
+                else:
+                    recieve_total += recieve
+
+            if recieve_total:
+                conn.sendall(recieve_total)
+
     except KeyboardInterrupt:
         conn.close()
 
