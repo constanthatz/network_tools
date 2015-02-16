f11f3223703fdac31801e4d75ea62bf804dd2b73
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-16 09:56:21 -0800

    add yield fixture to run echo server automatically without using another terminal.

--- test_echo.py
+++ test_echo.py
@@ -1,9 +1,9 @@
 from echo_client import client_socket_function
-import subprocess
+from echo_server import server_socket_function
 import pytest
 
 
-# @pytest.fixture(scope='module')
+# @pytest.yield_fixture(scope='module')
 # def server(request):
 #     """set up and tear down a server"""
 
@@ -14,24 +14,34 @@ import pytest
 
 #     request.addfinalizer(cleanup)
 
-#     return process
+#     yield process
+
+@pytest.yield_fixture(scope='module')
+def start_server():
+    """set up and tear down a server"""
+    import threading
+    target = server_socket_function
+    server_thread = threading.Thread(target=target)
+    server_thread.daemon = True
+    server_thread.start()
+    yield
 
 
-def test_client_socket_function_short():
+def test_client_socket_function_short(start_server):
     ''' 16 byte message. Short response'''
     message = "Can you hear me?"
     recieve = client_socket_function(message)
     assert recieve == message
 
 
-def test_client_socket_function_long():
+def test_client_socket_function_long(start_server):
     ''' 74 byte message. '''
     message = "Can you hear me? I am waiting for you to respond. Come on, where are you?!"
     recieve = client_socket_function(message)
     assert recieve == message
 
 
-def test_client_socket_function_unicode():
+def test_client_socket_function_unicode(start_server):
     ''' Unicode. '''
     message = u"Can you hear me? I am waiting for you to respond. Come on, where are you?!"
     recieve = client_socket_function(message)
