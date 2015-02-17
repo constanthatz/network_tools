52a6ab4167ea439221e0a2af02816f8368da9f5e
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-02-17 09:29:31 -0800

    clean up tests.

--- test_http.py
+++ test_http.py
@@ -1,9 +1,6 @@
 import http_client as ec
 import http_server as es
-from http_server import RequestError
 import pytest
-import email.utils
-import os
 
 
 @pytest.yield_fixture(scope='module')
@@ -19,95 +16,57 @@ def start_server():
 
 def test_response_ok():
     ''' Test ok response message. '''
-    first_line = 'HTTP/1.1 200 OK'
-    timestamp = 'Date: ' + email.utils.formatdate(usegmt=True)
-    content_header = 'Content-Type: text/plain'
-    body = '200 OK'
-    response_list = [first_line, timestamp, content_header, ' ', body, '\r\n']
-    assert es.response_ok() == '\r\n'.join(response_list)
+    body = '<li>Test</li>'
+    content_type = 'text/html'
+    info = (content_type, body)
+    actual = es.response_ok(info)
+    assert body in actual
+    assert content_type in actual
 
 
 def test_response_error():
     ''' Test error response message. '''
-    error_code = '404'
-    error_message = 'Not Found'
-    first_line = 'HTTP/1.1 {} {}'.format(error_code, error_message)
-    timestamp = 'Date: ' + email.utils.formatdate(usegmt=True)
-    content_header = 'Content-Type: text/plain'
-    body = '{} {}'.format(error_code, error_message)
-    response_list = [first_line, timestamp, content_header, ' ', body, '\r\n']
-    assert es.response_error(
-        error_code, error_message) == '\r\n'.join(response_list)
+    class error_test():
+        def __init__(self, code, msg):
+            self.code = code
+            self.msg = msg
+
+    error = error_test('404', 'Not Found')
+    actual = es.response_error(error)
+    assert error.code in actual
+    assert error.msg in actual
 
 
 def test_parse_request_uri():
     ''' Test parse request with a good request. '''
-    method = 'GET'
-    uri = '/index.html'
-    protocol = 'HTTP/1.1'
-    first_line = '{} {} {}'.format(method, uri, protocol)
-    timestamp = 'Date: ' + email.utils.formatdate(usegmt=True)
-    header = 'Host: www.example.com'
-    client_request = [first_line, timestamp, header, ' ', '\r\n']
-    assert es.parse_request('\r\n'.join(client_request)) == '/index.html'
+    client_request = 'GET /index.html HTTP/1.1\r\n'
+    assert es.parse_request(client_request) == '/index.html'
 
 
 def test_client_socket_function_ok():
-    ''' Test a good request'''
-    method = 'GET'
-    uri = '/index.html'
-    protocol = 'HTTP/1.1'
-    first_line = '{} {} {}'.format(method, uri, protocol)
-    timestamp = 'Date: ' + email.utils.formatdate(usegmt=True)
-    header = 'Host: www.example.com'
-    client_request = [first_line, timestamp, header, ' ', '\r\n']
+    ''' Test an OK response'''
+    client_request = 'GET / HTTP/1.1\r\n'
+    actual = ec.client_socket_function(client_request)
 
-    response = uri
-
-    recieve = ec.client_socket_function('\r\n'.join(client_request))
-    assert recieve == response
+    assert '200' in actual
+    assert 'OK' in actual
 
 
 def test_client_socket_function_405():
     ''' Test a 405 error'''
-    method = 'PUSH'
-    uri = '/index.html'
-    protocol = 'HTTP/1.1'
-    first_line = '{} {} {}'.format(method, uri, protocol)
-    timestamp = 'Date: ' + email.utils.formatdate(usegmt=True)
-    header = 'Host: www.example.com'
-    client_request = [first_line, timestamp, header, ' ', '\r\n']
+    client_request = 'PUSH /index.html HTTP/1.1\r\n'
+    error = {"code": '405', "msg": 'Method Not Allowed'}
+    actual = ec.client_socket_function(client_request)
 
-    error_code = '405'
-    error_message = 'Method Not Allowed'
-    first_line = 'HTTP/1.1 {} {}'.format(error_code, error_message)
-    timestamp = 'Date: ' + email.utils.formatdate(usegmt=True)
-    content_header = 'Content-Type: text/plain'
-    body = '{} {}'.format(error_code, error_message)
-    response_list = [first_line, timestamp, content_header, ' ', body, '\r\n']
-
-    recieve = ec.client_socket_function('\r\n'.join(client_request))
-    assert recieve == '\r\n'.join(response_list)
+    assert error['code'] in actual
+    assert error['msg'] in actual
 
 
 def test_client_socket_function_505():
     ''' Test a 505 error'''
-    method = 'GET'
-    uri = '/index.html'
-    protocol = 'HTTP/1.0'
-    first_line = '{} {} {}'.format(method, uri, protocol)
-    timestamp = 'Date: ' + email.utils.formatdate(usegmt=True)
-    header = 'Host: www.example.com'
-    client_request = [first_line, timestamp, header, ' ', '\r\n']
+    client_request = 'GET /index.html HTTP/1.0\r\n'
+    error = {"code": '505', "msg": 'HTTP Version Not Supported'}
+    actual = ec.client_socket_function(client_request)
 
-    error_code = '505'
-    error_message = 'HTTP Version Not Supported'
-    first_line = 'HTTP/1.1 {} {}'.format(error_code, error_message)
-    timestamp = 'Date: ' + email.utils.formatdate(usegmt=True)
-    content_header = 'Content-Type: text/plain'
-    body = '{} {}'.format(error_code, error_message)
-    response_list = [first_line, timestamp, content_header, ' ', body, '\r\n']
-
-    recieve = ec.client_socket_function('\r\n'.join(client_request))
-    assert recieve == '\r\n'.join(response_list)
-
+    assert error['code'] in actual
+    assert error['msg'] in actual
